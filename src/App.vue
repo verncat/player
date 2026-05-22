2e3a86d01c1fc15b3c48e4ca0e14449b602c9022
verncat <183478378+verncat@users.noreply.github.com>
2026-05-22 19:43:14 +0400

    fix: correct method invocation in reindex navigation item

--- src/App.vue
+++ src/App.vue
@@ -4594,7 +4594,7 @@ onUnmounted(() => {
           <svg viewBox="0 0 24 24" fill="currentColor" width="22" height="22"><path d="M10 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z"/></svg>
           Open Data Folder
         </a>
-        <a class="nav-item" href="#" @click.prevent="doReindex; showMobileNav = false">
+        <a class="nav-item" href="#" @click.prevent="doReindex(); showMobileNav = false">
           <svg viewBox="0 0 24 24" fill="currentColor" width="22" height="22"><path d="M17.65 6.35A7.958 7.958 0 0 0 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08A5.99 5.99 0 0 1 12 18c-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"/></svg>
           Reindex
         </a>
